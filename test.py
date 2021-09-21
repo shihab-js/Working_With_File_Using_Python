249ed752507f9c6e3296d59cc3e07384768076bd
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-21 14:04:30 +0600

    Something has been changed

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 # open a file read and write mode
-open_file = open('file1.txt', 'r+')
+open_file = open('file1.txt', 'r')
 
 # write something into the file
-open_file.write("Hello Boss")
+#open_file.write("Write Something! Boss.\n")
 
 # print successful message
 print('file open Successfully')
+
+# read a file and store data in variable
+content = open_file.read()
+
+# print the stored data
+print(content)
+
+# close the file
+open_file.close()
