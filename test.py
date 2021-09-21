780853b5955bba8e6cd8f389bc6422236b95dbf7
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-21 13:37:11 +0600

    comment hass been added

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+# open a file read and write mode
 open_file = open('file1.txt', 'r+')
+
+# write something into the file
 open_file.write("Hello Boss")
+
+# print successful message
 print('file open Successfully')
